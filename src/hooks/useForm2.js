469f5bcd407a1ad27444fc09dac0594a3a4d9fa3
Luis Ramirez <lar_8912@hotmail.com>
2024-09-12 13:22:10 -0600

    Bug from

--- src/hooks/useForm2.js
+++ src/hooks/useForm2.js
@@ -25,7 +25,7 @@ const useForm2 = (initialData, onValidate)=>{
 
         if (Object.keys(err).length === 0){
             setLoading(true)
-            fetch("https://formsubmit.co/ajax/luiscelayagto2209@gmail.com", {
+            fetch("https://formsubmit.co/ajax/casa.obrera.de.bajio@gmail.com", {
                 method: "POST",
                 headers: { 
                     'Content-Type': 'application/json',
